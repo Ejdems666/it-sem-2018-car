29d6256a4e7734da98026a010da22facdf5aeb64
Unknown <noe_luengocrisostomo@hotmail.com>
2018-05-22 12:25:28 +0200

    Sensors and motors update

---
+++ stateMachine/Motors/Motors.ino
@@ -0,0 +1,109 @@
+/
+/*  Remote control of two stepper motors
+ *  
+ *    0 - Stop both motors
+ *    1 - Both motors forward
+ *    2 - Both motors backward
+ *    3 - Turn left
+ *    4 - Turn right
+ */
+#include <AccelStepper.h>
+#include <ByteTransfer.h>
+
+const int motorAPin1 = 2;
+const int motorAPin2 = 3;
+const int motorAPin3 = 4;
+const int motorAPin4 = 5;
+const int motorBPin1 = 6;
+const int motorBPin2 = 7;
+const int motorBPin3 = 8;
+const int motorBPin4 = 9;
+// initialize the stepper library:
+AccelStepper stepperL(AccelStepper::FULL4WIRE, motorAPin1, motorAPin3, motorAPin2, motorAPin4);
+AccelStepper stepperR(AccelStepper::FULL4WIRE, motorBPin1, motorBPin3, motorBPin2, motorBPin4);
+
+const int inPin = 13;
+const int dataPin = 12;
+const int outPin = 11;
+
+uint8_t outData;
+boolean waitingToWrite;
+
+bool sendByte(uint8_t data)
+{
+  if(waitingToWrite) return false;
+  outData = data;
+  waitingToWrite = true;
+  return true;
+}
+
+void OnDataIn(uint8_t data)
+{
+  Serial.println(state);
+  switch(state) {
+        case 1:
+          goStraight();
+          sendByte(0);
+          break;
+        case 2:
+          turnRight();
+          sendByte(0);
+          break;
+        case 3:
+          turnLeft();
+          sendByte(0);
+          break;
+        default:
+          stepperL.setSpeed(0);
+          stepperR.setSpeed(0);
+          sendByte(1);
+          break;
+      }
+}
+
+bool OnDataOut(uint8_t &data)
+{
+  if(waitingToWrite)
+  {
+    data = outData;
+    waitingToWrite = false;
+    return true;
+  }
+  return false;
+}
+
+
+
+ByteTransfer bt(inPin, dataPin, outPin, OnDataIn, OnDataOut);
+
+void setup()
+{
+  waitingToWrite = false;
+  delay(10000);
+  bt.Initialize();
+  stepperL.setMaxSpeed(1000);  
+  stepperR.setMaxSpeed(1000); 
+}
+
+void loop()
+{
+  stepperL.runSpeed();
+  stepperR.runSpeed();
+  bt.Update();
+}
+
+void goStraight() {
+  stepperL.setSpeed(SPEED);
+  stepperR.setSpeed(-SPEED);
+}
+
+void turnRight() {
+  stepperL.setSpeed(SPEED);
+  stepperR.setSpeed(-SPEED * deccelerateSpeed);
+}
+
+void turnLeft() {
+  stepperL.setSpeed(SPEED);
+  stepperR.setSpeed(-SPEED * deccelerateSpeed);
+}
+
--- stateMachine/stateMachine.ino
+++ stateMachine/stateMachine.ino
@@ -1,18 +1,6 @@
+#include <ByteTransfer.h>
 #include <AccelStepper.h>
 #define HALFSTEP 8
-
-// Motor pin definitions
-#define motor1Pin1 2    // IN1 on the ULN2003 driver 1
-#define motor1Pin2 3    // IN2 on the ULN2003 driver 1
-#define motor1Pin3 4     // IN3 on the ULN2003 driver 1
-#define motor1Pin4 5     // IN4 on the ULN2003 driver 1
-
-
-// Motor pin definitions
-#define motor2Pin1 6    // IN1 on the ULN2003 driver 1
-#define motor2Pin2 7    // IN2 on the ULN2003 driver 1
-#define motor2Pin3 8     // IN3 on the ULN2003 driver 1
-#define motor2Pin4 9     // IN4 on the ULN2003 driver 1
 
 int photoSensorR = A1;
 int photoSensorL = A0;
@@ -21,25 +9,58 @@ int ledL = 10;
 int ledR = 11;
 
 int state = 0;
-#define thresholdR 920
-#define thresholdL 900
-int SPEED = 1000;
-#define deccelerateSpeed 0.5
 
+const int inPin = 11;//4
+const int dataPin = 12;//3
+const int outPin = 13; //2
 
-// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper with 28BYJ-48
-AccelStepper stepperL(HALFSTEP, motor1Pin1, motor1Pin3, motor1Pin2, motor1Pin4);
-AccelStepper stepperR(HALFSTEP, motor2Pin1, motor2Pin3, motor2Pin2, motor2Pin4);
+uint8_t outData;
+boolean waitingToWrite;
+
+void OnDataIn(uint8_t data)
+{
+  if(data == 0)
+  {
+    Serial.println("Response: OK");
+  }
+  else if(data == 1)
+  {
+    Serial.println("Unknown command");
+  }
+  else
+  {
+    Serial.print("Unknown response: ");
+    Serial.println(data);
+  }
+}
+
+bool OnDataOut(uint8_t &data)
+{
+  if(waitingToWrite)
+  {
+    data = outData;
+    waitingToWrite = false;
+    return true;
+  }
+  return false;
+}
+
+ByteTransfer bt(inPin, dataPin, outPin, OnDataIn, OnDataOut);
+
+String inString = "";
 
 
 void setup() {
   pinMode(photoSensorL, INPUT);
   pinMode(photoSensorR, INPUT);
+
+  pinMode(inPin, );
+  pinMode(dataPin, );
+  pinMode(outPin, );
+  
   pinMode(ledL, OUTPUT);
   pinMode(ledR, OUTPUT);
   Serial.begin(9600);
-  stepperL.setMaxSpeed(1000);  
-  stepperR.setMaxSpeed(1000); 
   digitalWrite(ledL, HIGH); 
   digitalWrite(ledR, HIGH); 
 }
@@ -60,14 +81,11 @@ void loop() {
           turnLeft();
           break;
         default:
-          stepperL.setSpeed(0);
-          stepperR.setSpeed(0);
           break;
       }
     }
   Serial.println(state);  
-  stepperL.runSpeed();
-  stepperR.runSpeed();
+  bt.Update();
   
 }
 
@@ -88,17 +106,3 @@ int checkState() {
   }
 }
 
-void goStraight() {
-  stepperL.setSpeed(SPEED);
-  stepperR.setSpeed(-SPEED);
-}
-
-void turnRight() {
-  stepperL.setSpeed(SPEED);
-  stepperR.setSpeed(-SPEED * deccelerateSpeed);
-}
-
-void turnLeft() {
-  stepperL.setSpeed(SPEED);
-  stepperR.setSpeed(-SPEED * deccelerateSpeed);
-}
