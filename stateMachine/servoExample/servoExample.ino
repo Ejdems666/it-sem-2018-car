28935da222ccb45f56ce029139f4c4c9c958300f
Adam Becvar <becvar.adam666@gmail.com>
2018-05-23 11:56:41 +0300

    Servo 3.0

--- stateMachine/servoExample/servoExample.ino
+++ stateMachine/servoExample/servoExample.ino
@@ -13,67 +13,45 @@ void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  
-  myservo.attach(9); // attaches the servo on pin 9 to the servo object
   myservo.write(90);
-
+  myservo.attach(9); // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-
-  // and the distance result in centimeters:
-  long cm = measure();
-  
-  if ( cm < 15){
-    myservo.write(180); // sets the servo position according to the scaled value
-    delay(4000);
-    cm = measure();
-    if (cm < 15){
-      myservo.write(0); // sets the servo position according to the scaled value
-      delay(4000);
+  if (straightIsBlocked()){
+    if (leftIsBlocked()){
+      
+    else {
+      
+    }
+    myservo.write(90);
+    delay(2000);
   }
-  myservo.write();
-  
-  
-  
-  Serial.print(cm);
-  
-  Serial.print("cm");
-  
-  Serial.println();
-  
-  delay(100);
-
 }
 
-long microsecondsToCentimeters(long microseconds) {
+boolean straightIsBlocked() {
+  return measure() < 15;
+}
 
-// The speed of sound is 340 m/s or 29 microseconds per centimeter.
-
-// The ping travels out and back, so to find the distance of the
-
-// object we take half of the distance travelled.
-
-return microseconds / 29 / 2;
-
+boolean leftIsBlocked() {
+  myservo.write(180);
+  delay(2000);
+  return measure() < 15;
 }
 
 long measure() {
   long duration, cm;
-  
   digitalWrite(trigPin, LOW);
-  
   delayMicroseconds(2);
-  
   digitalWrite(trigPin, HIGH);
-  
   delayMicroseconds(20);
-  
   digitalWrite(trigPin, LOW);
-
   duration = pulseIn(echoPin, HIGH);
-  
-  // convert the time into a distance
   
   return microsecondsToCentimeters(duration);  
 }
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
+}
+
